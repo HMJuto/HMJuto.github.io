63a19193645c3fe25e59e1ee44dfec445da39600
HeathermountB <bradleyholmes@heathermount.co.uk>
2026-03-17 20:54:05 +0000

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
 let bannerimage = document.getElementById("Banner-image");
-let bannerimages = ["Imagesforbanner/1.jpeg", "Imagesforbanner/2.jpeg", "Imagesforbanner/3.jpeg", "Imagesforbanner/4.jpeg", "Imagesforbanner/5.jpeg", "Imagesforbanner/6.jpeg", "Imagesforbanner/7.jpeg", "Imagesforbanner/8.jpeg", "Imagesforbanner/9.jpeg"];
+let bannerimages = ["Imagesforbanner/1.jpeg", "Imagesforbanner/2.jpeg", "Imagesforbanner/3.jpeg", "Imagesforbanner/4.jpeg", "Imagesforbanner/5.jpeg", "Imagesforbanner/6.jpeg", "Imagesforbanner/7.jpeg", "Imagesforbanner/8.jpeg"];
 
 let changebannerimage = () => {
-    let random = Math.floor(Math.random() * 9) + 1;
+    let random = Math.floor(Math.random() * 8) + 1;
     bannerimage.src = bannerimages[random]
 };
 
