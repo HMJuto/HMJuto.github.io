0d743aec5ab831475c2a5a5fdb32ed59b400fcba
HMJuto <hmyoungenterprise@heathermount.co.uk>
2026-06-19 09:19:38 +0100

    Update T-Shirts.js

--- T-Shirts.js
+++ T-Shirts.js
@@ -3,6 +3,6 @@ let Ordernowbtn = document.getElementById("Ordernowbtn");
 Ordernowbtn.addEventListener("click", GoToOnlineOrderservice);
 
 function GoToOnlineOrderservice() {
-  window.open("OnlineOrderservice.html")
-};
+  window.open("OnlineOrderservice.html");
+}
 
