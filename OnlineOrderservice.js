25ed4218502b10e60c3d0d6082fede8d74ddee5c
HMJuto <hmyoungenterprise@heathermount.co.uk>
2026-06-20 18:39:51 +0100

    Update OnlineOrderservice.js

--- OnlineOrderservice.js
+++ OnlineOrderservice.js
@@ -1,1 +1,13 @@
+let orderForm = document.getElementById("T_Shirt_product_order_form");
 
+let formData = new FormData();
+
+function submitForm() {
+  formData(orderForm)
+};
+
+orderForm.addEventListener('submit', submitForm)
+
+for (data in formData) {
+  console.log(data)
+};
