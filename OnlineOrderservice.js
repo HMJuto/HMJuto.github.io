252c9f405c0114ecd86f311504cca0b828a2bea4
HMJuto <hmyoungenterprise@heathermount.co.uk>
2026-06-20 19:39:40 +0100

    Update OnlineOrderservice.js

--- OnlineOrderservice.js
+++ OnlineOrderservice.js
@@ -8,6 +8,17 @@ function submitForm() {
 
 orderForm.addEventListener('submit', submitForm)
 
+let incRement = 0;
+let incRement1 = 0;
+
 for (data in formData) {
-  console.log(data)
+  console.log(data[incRement1, incRement]);
+  
+  if incRement >= 2 {
+    incRement1 = incRement1 + 1;
+  };
+
+  incRement = incRement + 1;
+  
 };
+
