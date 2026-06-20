13e908c15efa8151f55cc4210148b876e2f3d9bc
HMJuto <hmyoungenterprise@heathermount.co.uk>
2026-06-20 16:00:15 +0100

    Update OnlineOrderserviceresponse.js

--- OnlineOrderserviceresponse.js
+++ OnlineOrderserviceresponse.js
@@ -1,13 +1,13 @@
 let orderform = document.getElementById('Orderform')
 
-let Res1 = document.getElementById('res1')
-let Res2 = document.getElementById('res2')
-let Res3 = document.getElementById('res3')
+let Res1 = "OnlineOrderservice.html".getElementById('res1')
+let Res2 = "OnlineOrderservice.html".getElementById('res2')
+let Res3 = "OnlineOrderservice.html".getElementById('res3')
 
-let Lbl4 = document.getElementById('lbl4')
-let Lbl1 = document.getElementById('lbl1')
-let Lbl2 = document.getElementById('lbl2')
-let Lbl3 = document.getElementById('lbl3')
+let Lbl4 = "OnlineOrderservice.html".getElementById('lbl4')
+let Lbl1 = "OnlineOrderservice.html".getElementById('lbl1')
+let Lbl2 = "OnlineOrderservice.html".getElementById('lbl2')
+let Lbl3 = "OnlineOrderservice.html".getElementById('lbl3')
 
 let displaycontent = [
   [Lbl4.textContent], 
