291eff31cfecdecf8cf14f279daad52d6c5f6980
HMJuto <hmyoungenterprise@heathermount.co.uk>
2026-06-20 15:54:10 +0100

    Update OnlineOrderserviceresponse.js

--- OnlineOrderserviceresponse.js
+++ OnlineOrderserviceresponse.js
@@ -1,7 +1,19 @@
-let T_Shirtorderform = document.getElementById("Orderform")
+let orderform = document.getElementById('Orderform')
 
-let T_ShirtProduct_order_form = document.name("T_Shirt_product_order_form")
+let Res1 = document.getElementById('res1')
+let Res2 = document.getElementById('res2')
+let Res3 = document.getElementById('res3')
 
-T_Shirtorderform.textcontent = T_ShirtProduct_order_form.content
+let Lbl4 = document.getElementById('lbl4')
+let Lbl1 = document.getElementById('lbl1')
+let Lbl2 = document.getElementById('lbl2')
+let Lbl3 = document.getElementById('lbl3')
 
+let displaycontent = [
+  [Lbl4.textContent], 
+  [Lbl1.textContent, Res1.value], 
+  [Lbl2.textContent, Res2.value], 
+  [Lbl3.textContent, Res2.value]
+];
 
+orderform.innerHTML = displaycontent
