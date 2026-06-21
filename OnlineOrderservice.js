075f3b42e06299d075cc9d0bb9cef92f6aa398ca
HMJuto <hmyoungenterprise@heathermount.co.uk>
2026-06-21 14:21:40 +0100

    Update OnlineOrderservice.js

--- OnlineOrderservice.js
+++ OnlineOrderservice.js
@@ -1,5 +1,5 @@
 let orderForm = document.querySelector('form');
-let submitbtn = document.getElementById('SubmitBtn')
+let submitbtn = document.getElementById('SubmitBtn');
 
 let submitForm = submitbtn.addEventListener('click', function() {
   
