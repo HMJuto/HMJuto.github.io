f948a12876a9daef7a12b8ae210b4c9d0d92198d
HMJuto <hmyoungenterprise@heathermount.co.uk>
2026-09-18 21:20:02 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -16,7 +16,7 @@ T_Shirtsbtn.addEventListener("click", function() {
 
 let t_shirtImg = document.getElementById("T-Shirtsimage");
 let t_shirtPictures = ["T-ShirtsImg1.jpeg", "T-ShirtsImg2.jpeg", "T-ShirtsImg3.jpeg"];
-let body = document.getElementById("Body");
+let body = document.getElementById("body");
 
 window.addEventListener("load", function() {
     let TShirtImgChange = () => {
